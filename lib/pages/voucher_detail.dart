2557337ffa62f505a1d4c613417ce977440528e5
maevym <83373618+maevym@users.noreply.github.com>
2022-06-17 12:21:02 +0700

    nambahin_voucher_detail

--- lib/pages/voucher_detail.dart
+++ lib/pages/voucher_detail.dart
@@ -9,17 +9,70 @@ class VoucherDetailPage extends StatefulWidget {
   State<VoucherDetailPage> createState() => _VoucherDetailPageState();
 }
 
+showAlert(BuildContext context, String errorMessage) {
+  Widget okButton = TextButton(
+    child: Text("Ok"),
+    onPressed: () => Navigator.of(context).pop(),
+  );
+
+  AlertDialog alert = AlertDialog(
+    title: Text("Error"),
+    content: Text(errorMessage),
+    actions: [
+      okButton,
+    ],
+  );
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
+
+TextEditingController idController = TextEditingController();
+
 class _VoucherDetailPageState extends State<VoucherDetailPage> {
+  var _playerID = '';
+
+  void _submit() {
+    final playerID = idController.value.text;
+
+    if (playerID.isEmpty) {
+      showAlert(context, 'Player ID cannot be empty');
+    }
+
+    if (double.tryParse(playerID) != null) {
+      showAlert(context, 'Player ID is not numeric');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.selectedVoucher.game.name),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(widget.selectedVoucher.name),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.selectedVoucher.game.name),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image(
+                image: AssetImage(widget.selectedVoucher.game.image),
+                height: 300,
+              ),
+              Text(widget.selectedVoucher.name),
+              TextFormField(
+                controller: idController,
+                decoration: InputDecoration(
+                  labelText: 'Player ID',
+                  hintText: 'Enter Player ID',
+                ),
+                onChanged: (value) => setState(() => _playerID),
+              ),
+              ElevatedButton(onPressed: _submit, child: Text('Purchase'))
+            ],
+          ),
+        ));
   }
 }
